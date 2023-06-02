e2b29656a7144250bb93e9677c60138629ddd13a
Assiya Guenoune <assiyaguenoune19@gmail.com>
2023-06-02 17:52:38 +0100

    Change Script link

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
-const canvas = document.getElementById("canvas");
-const engine = new BABYLON.Engine(canvas, true);
+window.addEventListener('DOMContentLoaded', function(){
 
-// here the doc for Load function: //doc.babylonjs.com/typedoc/classes/babylon.sceneloader#load
-BABYLON.SceneLoader.Load("", "cheval.babylon", engine, function (scene) {
-  //as this .babylon example hasn't camera in it, we have to create one
-  const camera = new BABYLON.ArcRotateCamera("Camera", 1, 1, 4, BABYLON.Vector3.Zero(), scene);
-  camera.attachControl(canvas, false);
+  var canvas = document.getElementById('canvas');
 
-  scene.clearColor = new BABYLON.Color3(1, 1, 1);
-  //scene.ambientColor = new BABYLON.Color3.White();
+  var engine = new BABYLON.Engine(canvas, true);
+  var createScene = function(){
+      
+      var scene = new BABYLON.Scene(engine); //create scene
 
-  engine.runRenderLoop(function () {
-    scene.render();
-  });
+     
+      scene.clearColor = new BABYLON.Color3.White();  //set scene color
 
-  window.addEventListener("resize", function () {
-    engine.resize();
-  });
+      const camera = new BABYLON.ArcRotateCamera("Camera", 1, 1, 4, BABYLON.Vector3.Zero(), scene);  //add a camera 
+      camera.attachControl(canvas, false);
+      
+      const light = new BABYLON.PointLight("light",new BABYLON.Vector3(5,10,0), scene); // Lightting
+      light.parent= camera; // light color
+      light.diffuse = new BABYLON.Color3(1,1,1);
+
+      scene.actionManager= new BABYLON.ActionManager(scene);  //Ceate action
+      scene.actionManager.registerAction(
+          new BABYLON.ExecuteCodeAction(
+              {trigger : BABYLON.ActionManager.OnKeyUpTrigger, parameter : ""},
+              function (){
+                  light.setEnabled(!light.isEnabled());
+              }));
+
+      BABYLON.SceneLoader.ImportMesh("","","cheval.babylon",scene, //Load scene
+              function(newMeshes){
+                  newMeshes.forEach(function(mesh){
+                      mesh.rotation= new BABYLON.Vector3(
+                          BABYLON.Tools.ToRadians(45),0,0);
+                  })});           
+
+      return scene;
+  }
+
+  var scene = createScene();
+  engine.runRenderLoop(function(){
+      scene.render();
+
+  }); 
+
 });
