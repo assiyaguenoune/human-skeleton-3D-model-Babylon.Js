a452bcad18ad56e9cdf1507e3673be3a8a75b0af
Assiya Guenoune <assiyaguenoune19@gmail.com>
2023-06-02 20:40:31 +0100

    Add WebXR

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ window.addEventListener('DOMContentLoaded', function(){
       var scene = new BABYLON.Scene(engine); //create scene
 
      
-      scene.clearColor = new BABYLON.Color3.White();  //set scene color
+      scene.clearColor = new BABYLON.Color3.Black();  //set scene color
 
       const camera = new BABYLON.ArcRotateCamera("Camera", 1, 1, 4, BABYLON.Vector3.Zero(), scene);  //add a camera 
       camera.attachControl(canvas, false);
@@ -25,20 +25,40 @@ window.addEventListener('DOMContentLoaded', function(){
                   light.setEnabled(!light.isEnabled());
               }));
 
-      BABYLON.SceneLoader.ImportMesh("","","cheval.babylon",scene, //Load scene
+      const cheval=BABYLON.SceneLoader.ImportMesh("","","cheval.babylon",scene, 
               function(newMeshes){
                   newMeshes.forEach(function(mesh){
                       mesh.rotation= new BABYLON.Vector3(
                           BABYLON.Tools.ToRadians(45),0,0);
-                  })});           
-
+                  })});    
+      cheval.position = new BABYLON.Vector3(0,1,0);
+      const ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 4, height: 4},scene);
+      ground.position = new BABYLON.Vector3(0,0,0);
+      const xrPromise = scene.createDefaultXRExperienceAsync({
+        floorMeshes: [ground]
+    });
+      return xrPromise.then((xrExperience) => {
+        console.log("Done, WebXR is enabled.");
+        return scene;
+    });
+/*     const ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 4, height: 4});  
+      const xrPromise = scene.createDefaultXRExperienceAsync({
+        floorMeshes: [ground]
+    });    
+      return xrPromise.then((xrExperience) => {
+        console.log("Done, WebXR is enabled.");
+        return scene;
+  });*/
       return scene;
-  }
+  };
 
-  var scene = createScene();
+  /*var scene = createScene();
   engine.runRenderLoop(function(){
       scene.render();
 
-  }); 
+  }); */
+  createScene().then(sceneToRender => {
+    engine.runRenderLoop(() => sceneToRender.render());
+});
 
 });
